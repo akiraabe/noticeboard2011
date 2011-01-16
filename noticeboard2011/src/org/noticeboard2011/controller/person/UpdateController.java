db746dbd27e38bd1b48b7c2e6b0aca3793e50210
akiraabe2005 <akiraabe2005@yahoo.co.jp>
2011-01-16 04:29:44 +0000

    Edit機能追加

--- noticeboard2011/src/org/noticeboard2011/controller/person/UpdateController.java
+++ noticeboard2011/src/org/noticeboard2011/controller/person/UpdateController.java
@@ -1,10 +1,13 @@
 package org.noticeboard2011.controller.person;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.noticeboard2011.controller.AbstractJsonController;
 import org.noticeboard2011.service.PersonService;
-import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
+import org.slim3.controller.validator.Validators;
 
 /**
  * {basePath}/update のURLによって呼ばれるコントローラ<br/>
@@ -14,7 +17,7 @@ import org.slim3.controller.Navigation;
  * 
  * @author akiraabe
  */
-public class UpdateController extends Controller {
+public class UpdateController extends AbstractJsonController {
     
     static Logger logger = Logger.getLogger(UpdateController.class.getName());
     private PersonService personService = new PersonService();
@@ -23,13 +26,38 @@ public class UpdateController extends Controller {
     public Navigation run() throws Exception {
         
         logger.fine("UpdateController#run start.");
+        logger.fine("command : " + request.getParameter("command"));
         logger.fine("firstName : " + request.getParameter("firstName"));
+        logger.fine("lastName : " + request.getParameter("lastName"));
         logger.fine("id : " + request.getParameter("id"));
         logger.fine("place : " + request.getParameter("place"));
         
-        personService.update(new Long(request.getParameter("id")), request.getParameter("place"));
+        Map<String, String> map = new HashMap<String, String>();
+        if (!validate() && "PROPERTY".equals(request.getParameter("command"))) {
+            //TODO エラー処理
+            map.put("returncode", "INVALID");
+            logger.warning("VALIDATION ERROR");
+            json(map);
+            return null;
+        } else {
+            map.put("returncode", "VALID");
+        }
         
+        if ("PLACE".equals(request.getParameter("command")))
+            personService.updatePlace(new Long(request.getParameter("id")), request.getParameter("place"));
+        
+        if ("PROPERTY".equals(request.getParameter("command")))
+            personService.updateProperty(new Long(request.getParameter("id")), request.getParameter("firstName"), request.getParameter("lastName"));
+
+        json(map);
         return null;
+    }
+    
+    private boolean validate() {
+        Validators v = new Validators(request);
+        v.add("firstName", v.required());
+        v.add("lastName", v.required());
+        return v.validate();
     }
 
 }
--- noticeboard2011/src/org/noticeboard2011/service/PersonService.java
+++ noticeboard2011/src/org/noticeboard2011/service/PersonService.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.noticeboard2011.meta.PersonMeta;
 import org.noticeboard2011.model.Person;
 import org.slim3.datastore.Datastore;
+import org.slim3.datastore.ModelQuery;
 import org.slim3.util.BeanUtil;
 
 import com.google.appengine.api.datastore.Key;
@@ -20,26 +21,30 @@ public class PersonService {
         return Datastore.get(personMeta, key, version);
     }
 
-    public List<Person> getPersonList(int offset, int limit) {
-        return Datastore
+    public List<Person> getPersonList(int offset, int limit, String query, String sortorder) {
+        ModelQuery<Person> modelQuery = 
+        Datastore
             .query(personMeta)
-            .sort(personMeta.firstName.asc)
             .offset(offset)
-            .limit(limit)
-            .asList();
+            .limit(limit);
+        if (!"".equals(query)) {
+            modelQuery = modelQuery.filter(personMeta.firstName.startsWith(query));
+        }
+        if ("desc".equals(sortorder)) {
+            modelQuery = modelQuery.sort(personMeta.firstName.desc);
+        } else {
+            modelQuery = modelQuery.sort(personMeta.firstName.asc);
+        }
+        return modelQuery.asList();
     }
 
-    public List<Person> getPersonListDesc(int offset, int limit) {
-        return Datastore
-            .query(personMeta)
-            .sort(personMeta.firstName.desc)
-            .offset(offset)
-            .limit(limit)
-            .asList();
-    }
-
-    public Integer count() {
-        return Datastore.query(personMeta).count();
+    public Integer count(String query) {
+        if (!"".equals(query)) {
+            return Datastore.query(personMeta).filter(personMeta.firstName.startsWith(query)).count();
+        } else {
+            return Datastore.query(personMeta).count();
+        }
+        
     }
 
     public Person insert(Map<String, Object> input) {
@@ -53,9 +58,16 @@ public class PersonService {
         return person;
     }
 
-    public void update(Long id, String place) {
+    public void updatePlace(Long id, String place) {
         Person person = Datastore.get(Person.class, KeyFactory.createKey("Person", id));
         person.setPlace(place);
+        Datastore.put(person);
+    }
+
+    public void updateProperty(Long id, String firstName, String lastName) {
+        Person person = Datastore.get(Person.class, KeyFactory.createKey("Person", id));
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
         Datastore.put(person);
     }
 }
