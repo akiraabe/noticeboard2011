4b59fc81160bf91675394d386bc59b773dc63407
akiraabe2005@yahoo.co.jp <akiraabe2005@yahoo.co.jp@372c3d40-5f9c-236e-5e44-b6814fbb4733>
2011-01-30 21:57:08 +0000

    Add TestCase.

--- trunk/noticeboard2011/test/org/noticeboard2011/controller/person/UpdateControllerTest.java
+++ trunk/noticeboard2011/test/org/noticeboard2011/controller/person/UpdateControllerTest.java
@@ -1,17 +1,101 @@
 package org.noticeboard2011.controller.person;
 
-import org.slim3.tester.ControllerTestCase;
-import org.junit.Test;
-import org.noticeboard2011.controller.person.UpdateController;
-
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.noticeboard2011.model.Person;
+import org.noticeboard2011.service.PersonService;
+import org.slim3.tester.ControllerTestCase;
+
+/**
+ * UpdateControllerのテスト
+ * 
+ * @author akiraabe
+ *
+ */
 public class UpdateControllerTest extends ControllerTestCase {
 
+    private Person person = null;
+    
+    /**
+     * テストデータのセットアップ
+     */
+    @Before
+    public void init() {
+        PersonService service = new PersonService();
+        Map<String, Object> input = new HashMap<String, Object>();
+        input.put("firstName", "James");
+        input.put("lastName", "Brown");
+        input.put("place", "Absent");
+        person = service.insert(input);
+    }
+    
+    
+    /**
+     * 場所の更新の正常ケース<br/>
+     * <br/>
+     * 合格条件
+     * <li>Validationに通り、遷移先画面にnullが指定される</li>
+     */
     @Test
-    public void run() throws Exception {
+    public void normalCaseByCommandPlace() throws Exception {
+        tester.param("firstName", "James");
+        tester.param("lastName", "Brown");
+        tester.param("place", "Absent");
+        tester.param("command", "PLACE");
+//        System.out.println(person.getKey().getId());
+        tester.param("id", person.getKey().getId());
         tester.start("/person/update");
+
+        UpdateController controller = tester.getController();
+        assertThat(controller, is(notNullValue()));
+        assertThat(tester.isRedirect(), is(false));
+        assertThat(tester.getDestinationPath(), is(nullValue()));
+    }
+    
+    /**
+     * 属性の更新の正常ケース<br/>
+     * <br/>
+     * 合格条件
+     * <li>Validationに通り、遷移先画面にnullが指定される</li>
+     */
+    @Test
+    public void normalCaseByCommandProperty() throws Exception {
+        tester.param("firstName", "James");
+        tester.param("lastName", "Brown");
+        tester.param("place", "Absent");
+        tester.param("command", "PROPERTY");
+//        System.out.println(person.getKey().getId());
+        tester.param("id", person.getKey().getId());
+        tester.start("/person/update");
+
+        UpdateController controller = tester.getController();
+        assertThat(controller, is(notNullValue()));
+        assertThat(tester.isRedirect(), is(false));
+        assertThat(tester.getDestinationPath(), is(nullValue()));
+    }
+    
+    /**
+     * 属性の更新のエラーケース<br/>
+     * <br/>
+     * 合格条件
+     * <li>Validationに通り、遷移先画面にnullが指定される</li>
+     */
+    @Test
+    public void errorCaseByCommandProperty() throws Exception {
+        tester.param("firstName", "");
+        tester.param("lastName", "Brown");
+        tester.param("place", "Absent");
+        tester.param("command", "PROPERTY");
+//        System.out.println(person.getKey().getId());
+        tester.param("id", person.getKey().getId());
+        tester.start("/person/update");
+
         UpdateController controller = tester.getController();
         assertThat(controller, is(notNullValue()));
         assertThat(tester.isRedirect(), is(false));
--- trunk/noticeboard2011/test/org/noticeboard2011/model/PersonTest.java
+++ trunk/noticeboard2011/test/org/noticeboard2011/model/PersonTest.java
@@ -1,18 +1,36 @@
 package org.noticeboard2011.model;
 
-import org.slim3.tester.AppEngineTestCase;
-import org.junit.Test;
-import org.noticeboard2011.model.Person;
-
-import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+import org.slim3.tester.AppEngineTestCase;
+
+/**
+ * Personモデルのテスト
+ * 
+ * @author akiraabe
+ *
+ */
 public class PersonTest extends AppEngineTestCase {
 
     private Person model = new Person();
 
+    /**
+     * Personモデルのテスト<br/>
+     * <br/>
+     * 合格条件<br/>
+     * <li>セットした値が取り出せること</li>
+     * @throws Exception
+     */
     @Test
     public void test() throws Exception {
         assertThat(model, is(notNullValue()));
+        model.setFirstName("James");
+        model.setLastName("Brown");
+        model.setPlace("Absent");
+        assertThat(model.getFirstName(), is("James"));
+        assertThat(model.getLastName(), is("Brown"));
+        assertThat(model.getPlace(), is("Absent"));
     }
 }
